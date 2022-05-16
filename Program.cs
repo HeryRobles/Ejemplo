b8e3603a508c4f7afe8df50bbee4ef559e8ffca7
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-05-16 12:14:09 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//namespace Ejemplo
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            List<string> nombres = new List<string>(); 
+//        }
+//    }
+//}
+
+//string cadena = "Hola|MUndo|Heriberto";
+
